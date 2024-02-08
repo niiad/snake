c35946e1c59f439cb85d050f44f467b52ffc7cb4
ghost <samuelbrownai@gmail.com>
2024-02-08 16:19:47 -0500

    init main class

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 from settings import *
+
+
+class Main:
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+    def run(self) -> None:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            pygame.display.update()
+
+
+main = Main()
+
+main.run()
